58206bfc34c1366da4f2e8f15f565ffb8473d3b3
DannyBabe <daniel.mustaine@usa.net>
2019-09-08 02:41:52 +1000

    Working Coords

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function jaavaa() {
-
+    
     alert("greetings friend");
 }
 
@@ -8,204 +8,258 @@ function searchTransport() {
     var directionsRenderer = new google.maps.DirectionsRenderer;
     var directionsService = new google.maps.DirectionsService;
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 14,
-      center: {lat: 37.77, lng: -122.447}
+        zoom: 7,
+        center: {lat: -35.12, lng: 149.12}
     });
     directionsRenderer.setMap(map);
-
+    
     calculateAndDisplayRoute(directionsService, directionsRenderer);
     document.getElementById('mode').addEventListener('change', function() {
-      calculateAndDisplayRoute(directionsService, directionsRenderer);
+        calculateAndDisplayRoute(directionsService, directionsRenderer, );
     });
-  }
+}
 
-  function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
     var selectedMode = document.getElementById('mode').value;
+    
+    //var terst = new google.maps.Geocoder()
+    //console.log(terst);
+    
+    //var markerOrigin = geocodeAddress(document.getElementById("autocomplete").value, map, 1);
+    //var markerDest = geocodeAddress(document.getElementById("autocompleteDest").value, map, 0);
+    
+    var markerOrigin = codeAddress(document.getElementById("autocomplete").value);
+    
+    console.log("1 " + markerOrigin);
+    //var markerDest = codeAddress(document.getElementById("autocompleteDest").value);
+    var markerDest = codeAddress("81 Stonehaven Crescent, Deakin ACT, Australia");
+    console.log("2 " + markerDest);
+    
+    console.log(markerOrigin);
+    console.log(markerDest);
 
-    var terst = Geocoder.geocode();
-    console.log(terst);
-
-    var markerOrigin = geocodeAddress(document.getElementById("autocomplete").value, map, 1);
-    var markerDest = geocodeAddress(document.getElementById("autocompleteDest").value, map, 0);
-
-        if (markerOrigin !== undefined) {
-            directionsService.route({
+    document.getElementById("autocomplete").value
+    
+    
+    if (markerOrigin !== undefined) {
+        directionsService.route({
             origin: {lat: markerOrigin.position.lat, lng: markerOrigin.position.lng},  // Haight.
-            destination: {lat: markerOriginDest.position.lat, lng: markerOriginDest.position.lng},  // Ocean Beach.
+            destination: {lat: markerDest.position.lat, lng: markerDest.position.lng},  // Ocean Beach.
             // Note that Javascript allows us to access the constant
             // using square brackets and a string value as its
             // "property."
             travelMode: google.maps.TravelMode[selectedMode]
-            }, function(response, status) {
+        }, function(response, status) {
             if (status == 'OK') {
                 directionsRenderer.setDirections(response);
             } else {
                 window.alert('Directions request failed due to ' + status);
             }
-            });
-            
-        }
-  
-  }
-
-  
-  function geocodeAddress(geocoder, resultsMap, oriDest) {
-    
-    if (oriDest == 1) {var address = document.getElementById('autocomplete').value;}
-    else { document.getElementById("autocompleteDest") }
-
-
-    geocoder.geocode({'address': address}, function(results, status) {
-      if (status === 'OK') {
-        resultsMap.setCenter(results[0].geometry.location);
-        
-        
-        var marker = new google.maps.Marker({
-          map: resultsMap,
-          position: results[0].geometry.location  
         });
-
-        return marker;
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-        return undefined;
-      }
-      
-
-    });
-  }
-
-
-//   function geolocate() {
-
-//         var geolocation = {
-//           lat: -35.308030,
-//           lng: 149.124438
-//         };
-//         var circle = new google.maps.Circle(
-//             {center: geolocation, radius: 100});
-//         autocomplete.setBounds(circle.getBounds());
-      
-//   }
-
-
-function initInitMap() {
-
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -35.308402, lng: 149.124433},
-        zoom: 8
-      });
-
+    
+    }
+    
 }
 
+function codeAddress(address) 
+{
+    console.log(address);
+    var geocoder= new google.maps.Geocoder();
 
+    geocoder.geocode( {address:address}, function(results, status) 
+    {
+        if (status == google.maps.GeocoderStatus.OK) 
+        {
+            var tempLat = results[0].geometry.bounds.na.g.toString();
+            var tempLng = results[0].geometry.bounds.ja.g.toString();
 
-function initializeGMAPI() {
-    
-
-    initAutocomplete();
-    initDestAutoComplete(); 
-    initInitMap();
-
- }
-
-
-
-            // This sample uses the Autocomplete widget to help the user select a
-            // place, then it retrieves the address components associated with that
-            // place, and then it populates the form fields with those details.
-            // This sample requires the Places library. Include the libraries=places
-            // parameter when you first load the API. For example:
-            // <script
-            // src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-            
-            var placeSearch, autocomplete;
-            
-            var componentForm = {
-              street_number: 'short_name',
-              route: 'long_name',
-              locality: 'long_name',
-              administrative_area_level_1: 'short_name',
-              country: 'long_name',
-              postal_code: 'short_name'
+            var convertCoords = {
+                lat: parseFloat(tempLat.substring(0,10)), 
+                lng: parseFloat(tempLng.substring(0,10))
             };
-            
-            function initAutocomplete() {
 
-            
-              // Create the autocomplete object, restricting the search predictions to
-              // geographical location types.
-              autocomplete = new google.maps.places.Autocomplete(
-                  document.getElementById('autocomplete'), {types: ['geocode']});
-            
-              // Avoid paying for data that you don't need by restricting the set of
-              // place fields that are returned to just the address components.
-              autocomplete.setFields(['address_component']);
-            
-              // When the user selects an address from the drop-down, populate the
-              // address fields in the form.
-              //autocomplete.addListener('place_changed', fillInAddress);
+            map.setCenter(convertCoords);//center the map over the result
 
-              //initDestAutoComplete();
+            //place a marker at the location
+            var marker = new google.maps.Marker({
+                map: map,
+                position: convertCoords
+            });
+            console.log(marker);
+            console.log(marker);
 
+            //return marker;
+            return convertCoords;
+        }  
+        else 
+        {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+    
+    
+    function geocodeAddress(geocoder, resultsMap, oriDest) {
+        
+        if (oriDest == 1) {var address = document.getElementById('autocomplete').value;}
+        else { document.getElementById("autocompleteDest") }
+        
+        
+        geocoder.geocode({'address': address}, function(results, status) {
+            if (status === 'OK') {
+                resultsMap.setCenter(results[0].geometry.location);
+                
+                
+                var marker = new google.maps.Marker({
+                    map: resultsMap,
+                    position: results[0].geometry.location  
+                });
+                
+                return marker;
+            } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+                return undefined;
             }
             
-            function initDestAutoComplete() {
-
-                // Create the autocomplete object, restricting the search predictions to
-                // geographical location types.
-                autocompleteDest = new google.maps.places.Autocomplete(
-                    document.getElementById('autocompleteDest'), {});
-              
+            
+        });
+    }
+    
+    
+    //   function geolocate() {
+    
+    //         var geolocation = {
+    //           lat: -35.308030,
+    //           lng: 149.124438
+    //         };
+    //         var circle = new google.maps.Circle(
+    //             {center: geolocation, radius: 100});
+    //         autocomplete.setBounds(circle.getBounds());
+    
+    //   }
+    
+    
+    function initInitMap() {
+        
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -35.308402, lng: 149.124433},
+            zoom: 8
+        });
+        
+    }
+    
+    
+    
+    function initializeGMAPI() {
+        
+        
+        initAutocomplete();
+        initDestAutoComplete(); 
+        initInitMap();
+        
+        
+        
+    }
+    
+    
+    
+    // This sample uses the Autocomplete widget to help the user select a
+    // place, then it retrieves the address components associated with that
+    // place, and then it populates the form fields with those details.
+    // This sample requires the Places library. Include the libraries=places
+    // parameter when you first load the API. For example:
+    // <script
+    // src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+    
+    var placeSearch, autocomplete;
+    
+    var componentForm = {
+        street_number: 'short_name',
+        route: 'long_name',
+        locality: 'long_name',
+        administrative_area_level_1: 'short_name',
+        country: 'long_name',
+        postal_code: 'short_name'
+    };
+    
+    function initAutocomplete() {
+        
+        
+        // Create the autocomplete object, restricting the search predictions to
+        // geographical location types.
+        autocomplete = new google.maps.places.Autocomplete(
+            document.getElementById('autocomplete'), {types: ['geocode']});
+            
+            // Avoid paying for data that you don't need by restricting the set of
+            // place fields that are returned to just the address components.
+            autocomplete.setFields(['address_component']);
+            
+            // When the user selects an address from the drop-down, populate the
+            // address fields in the form.
+            //autocomplete.addListener('place_changed', fillInAddress);
+            
+            //initDestAutoComplete();
+            
+        }
+        
+        function initDestAutoComplete() {
+            
+            // Create the autocomplete object, restricting the search predictions to
+            // geographical location types.
+            autocompleteDest = new google.maps.places.Autocomplete(
+                document.getElementById('autocompleteDest'), {});
+                
                 // Avoid paying for data that you don't need by restricting the set of
                 // place fields that are returned to just the address components.
                 autocompleteDest.setFields(['address_component']);
-              
+                
                 // When the user selects an address from the drop-down, populate the
                 // address fields in the form.
                 //autocomplete.addListener('place_changed', fillInAddress);
-  
-              }
-
-
+                
+            }
+            
+            
             function fillInAddress() {
-              // Get the place details from the autocomplete object.
-              var place = autocomplete.getPlace();
-            
-              for (var component in componentForm) {
-                document.getElementById(component).value = '';
-                document.getElementById(component).disabled = false;
-              }
-            
-              // Get each component of the address from the place details,
-              // and then fill-in the corresponding field on the form.
-              for (var i = 0; i < place.address_components.length; i++) {
-                var addressType = place.address_components[i].types[0];
-                if (componentForm[addressType]) {
-                  var val = place.address_components[i][componentForm[addressType]];
-                  document.getElementById(addressType).value = val;
+                // Get the place details from the autocomplete object.
+                var place = autocomplete.getPlace();
+                
+                for (var component in componentForm) {
+                    document.getElementById(component).value = '';
+                    document.getElementById(component).disabled = false;
                 }
-              }
+                
+                // Get each component of the address from the place details,
+                // and then fill-in the corresponding field on the form.
+                for (var i = 0; i < place.address_components.length; i++) {
+                    var addressType = place.address_components[i].types[0];
+                    if (componentForm[addressType]) {
+                        var val = place.address_components[i][componentForm[addressType]];
+                        document.getElementById(addressType).value = val;
+                    }
+                }
             }
             
             // Bias the autocomplete object to the user's geographical location,
             // as supplied by the browser's 'navigator.geolocation' object.
             function geolocate() {
-
+                
                 var geolocation = {
-                lat: -35.308030,
-                lng: 149.124438
+                    lat: -35.308030,
+                    lng: 149.124438
                 };
                 var circle = new google.maps.Circle(
-                {center: geolocation, radius: 2});
-                autocomplete.setBounds(circle.getBounds());
-
-            }
-
-
-
-// function searchTransport() {
-
-
-
-// }
+                    {center: geolocation, radius: 2});
+                    autocomplete.setBounds(circle.getBounds());
+                    
+                }
+                
+                
+                
+                // function searchTransport() {
+                
+                
+                
+                // }
